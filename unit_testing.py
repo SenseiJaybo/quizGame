79937e19214270508c631c0094d3423e927eb499
SenseiJaybo <SenseiJaybob@gmail.com>
2022-12-02 11:14:32 +0000

    created unit testing file

---
+++ unit_testing.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class MainTestCase(unittest.TestCase):
+    def testKeyRepeat(self):
+        self.assertEqual(True, False)
+
+    def testFrameRate(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
