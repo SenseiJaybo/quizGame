cc539721064e0dd47df08e2365ae86a6071c6f34
SenseiJaybo <SenseiJaybob@gmail.com>
2022-12-02 10:35:14 +0000

    comments added

--- Main.py
+++ Main.py
@@ -2,17 +2,23 @@ import pygame
 import sqlite3
 from Main_class import *
 
+# load main class
 m = Main()
 m.__init__()
 
+# while loop to run program
 running = True
 while running:
 
+    # look at all events
     for event in pygame.event.get():
+        # exit if 'x' button pressed
         if event.type == pygame.QUIT:
             quit()
+        # exit if 'ESC' pressed
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 quit()
 
+    # update game window
     m.update()
--- Main_class.py
+++ Main_class.py
@@ -4,10 +4,13 @@ import pygame
 class Main:
     def __init__(self):
         pygame.init()
+        # set a key to automatically repeat if it is held down
         pygame.key.set_repeat(200, 20)
+        # create screen and clock
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.clock = pygame.time.Clock()
 
     def update(self):
+        # set frame rate
         self.clock.tick(24)
         pygame.display.update()
