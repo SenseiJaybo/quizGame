391530a89e16f72175c4038104012b36f8d98db7
SenseiJaybo <SenseiJaybob@gmail.com>
2022-12-03 09:35:07 +0000

    commented sprite and added unit tests

--- Sprite.py
+++ Sprite.py
@@ -5,14 +5,18 @@ class Sprite:
     def __init__(self, x, y, scale, image):
         self.X = x
         self.Y = y
+        # load image and scale it accordingly
         self.image = pygame.image.load(f'{image}')
         self.image = pygame.transform.scale(self.image,
                                             (self.image.get_width() * scale, self.image.get_height() * scale))
+        # collision box
         self.rect = self.image.get_rect()
         self.show = True
 
     def draw(self, surface):
+        # makes sure that if the sprite moves then the collision box will move as well
         self.rect.move(self.X, self.Y)
+        # draws the sprite if it is meant to be visible
         if self.show:
             surface.blit(self.image, (self.X, self.Y))
 
