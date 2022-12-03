4036c8753434b79ee9c05e2b9a14c0838b3a30bc
SenseiJaybo <SenseiJaybob@gmail.com>
2022-12-03 10:24:02 +0000

    custom error file

--- Sprite.py
+++ Sprite.py
@@ -1,10 +1,22 @@
 import pygame
+from Main_class import *
+from Errors import CustomError
+
+m = Main()
 
 
 class Sprite:
     def __init__(self, x, y, scale, image):
-        self.X = x
-        self.Y = y
+        # check that X is in screen confines
+        if x >= m.screen.get_width() or x <= 0:
+            raise CustomError("X value must be within screen confines")
+        else:
+            self.X = x
+        # check that Y is in screen confines
+        if y >= m.screen.get_height() or y <= 0:
+            raise CustomError("Y value must be within screen confines")
+        else:
+            self.Y = y
         # load image and scale it accordingly
         self.image = pygame.image.load(f'{image}')
         self.image = pygame.transform.scale(self.image,
@@ -19,4 +31,3 @@ class Sprite:
         # draws the sprite if it is meant to be visible
         if self.show:
             surface.blit(self.image, (self.X, self.Y))
-
